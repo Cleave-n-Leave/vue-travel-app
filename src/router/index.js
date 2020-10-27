dc5cd51cc2fb69b3af4c3e678a7cd2a534999b4d
Andrew Heyer <andrewbheyer@gmail.com>
2020-10-26 20:18:28 -0400

    initializing branch

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Brazil from "../views/Brazil.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: Brazil
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
